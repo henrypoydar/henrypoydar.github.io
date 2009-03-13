6979568ade3337e4d02d996b56ecf9dcbf1d3317
hpoydar <hpoydar@gmail.com>
2009-03-13 18:15:37 -0400

    Added more circle cuts

--- javascripts/application.js
+++ javascripts/application.js
@@ -12,15 +12,6 @@ function drawCanvasTag(text) {
   
 }
 
-// A rand() helper
-function rand( min, max ) {
-  if(typeof(max) == 'undefined') {
-    var max = min;
-    min = 0;
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 $(document).ready(function() {
   // Draw canvas art
   $('canvas#art').setupCircleCuts();
