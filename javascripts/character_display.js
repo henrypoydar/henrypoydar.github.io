abf825eb5559ca5a8d8c73de7dffcfda08913f1a
hpoydar <hpoydar@gmail.com>
2009-04-04 14:53:20 -0400

    Added rudimentary letter and alphabet display

---
+++ javascripts/character_display.js
@@ -0,0 +1,16 @@
+function setupChars(chars_array) {
+  var h = $(window).height() / 3;
+  $('body').css('font-size', h + 'px');
+  $.each(chars_array, function(i) {
+    $('#chars').append('<div>' + chars_array[i] + '</div>');
+  });
+}
+
+function displayChars(chars_array, interval) {
+   var index = 0;
+   setInterval(function(){
+     $('#chars > div:eq(' + ((index == 0) ? 0 : index - 1) + ')').slideUp('fast');
+     $('#chars > div:eq(' + index + ')').show();
+     index = (index < chars_array.length) ? index + 1 : 0;
+   }, interval);
+ }
