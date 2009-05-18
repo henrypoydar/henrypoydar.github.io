5b57ae3bdaa889b7627f688d5dd9f7ee13ea2a42
hpoydar <hpoydar@gmail.com>
2009-05-18 09:17:31 -0400

    Updated processing file selector code

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,13 +1,20 @@
-// Apply a processing file to a jQuery-selected (canvas) context
-// the 'prefixes' argument is an array of strings to add to the 
-// top of the processing file before it's eval'd
 (function($) {
-  $.fn.apply_processing = function(processing_file, prefixes) {
-    prefixes = (typeof(prefixes) != 'undefined' ? prefixes : []);
-    var ctx = this[0];
-    $.get(processing_file, function(data){ 
-      Processing(ctx, (prefixes.join('\n') + '\n' + data));
+  $.fn.render_processing = function(processing_file, options) {
+    
+    var defaults = {  
+      prefix: '',
+      suffix: '',
+    };
+    
+    var options = $.extend(defaults, options);
+    
+    return this.each(function() {
+      ctx = this;
+      $.get(processing_file, function(data){ 
+        Processing(ctx, [options.prefix, data, options.suffix].join(''));
+      });
     });
+    
   }
 })(jQuery); 
 
@@ -16,12 +23,13 @@ Cufon.replace('body', { hover: true });
 
 // On document ready ...
 $(document).ready(function() {
-  
-  var prefixes = [];
-  prefixes.push('int screen_width = ' + $(window).width() + ';');
-  prefixes.push('int screen_height = ' + $(window).height() + ';');
-  
-  $('canvas#art').apply_processing('processing/circle_cuts.pde', prefixes);
-  $(window).resize(function() { location.reload(); });
 
+  $('canvas#art').render_processing('processing/circle_cuts.pde', { 
+     prefix: 
+      "int screen_width = " + $(window).width() + ";" +
+      "int screen_height = " + $(window).height() + ";"
+   });
+  
+  $(window).resize(function() { location.reload(); });
+ 
 });
