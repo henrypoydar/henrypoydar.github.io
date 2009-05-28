92918fb2cab1e20940ffffc3642f8a75039b1b2f
hpoydar <hpoydar@gmail.com>
2009-05-28 12:22:31 -0400

    Added pde_loader plugin

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,27 +1,12 @@
-// Apply a processing file to a jQuery-selected (canvas) context
-// the 'prefixes' argument is an array of strings to add to the 
-// top of the processing file before it's eval'd
-(function($) {
-  $.fn.apply_processing = function(processing_file, prefixes) {
-    prefixes = (typeof(prefixes) != 'undefined' ? prefixes : []);
-    var ctx = this[0];
-    $.get(processing_file, function(data){ 
-      Processing(ctx, (prefixes.join('\n') + '\n' + data));
-    });
-  }
-})(jQuery); 
-
-// Typography...
 Cufon.replace('body', { hover: true });
 
-// On document ready ...
 $(document).ready(function() {
   
-  var prefixes = [];
-  prefixes.push('int screen_width = ' + $(window).width() + ';');
-  prefixes.push('int screen_height = ' + $(window).height() + ';');
+  var screen_dimensions = 
+    'int screen_width = ' + $(window).width() + ';' +
+    'int screen_height = ' + $(window).height() + ';';
   
-  $('canvas#art').apply_processing('processing/circle_cuts.pde', prefixes);
+  $('canvas#background').load_pde('processing/circle_cuts.pde', {prefix: screen_dimensions});
   $(window).resize(function() { location.reload(); });
 
 });
